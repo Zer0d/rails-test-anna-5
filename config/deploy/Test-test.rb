635839067104bdcf12506368699119810889e646
zerod <zerod@dius.com.au>
2012-10-08 01:36:49 +1100

    Initial zerod configuration

--- config/deploy/Test-test.rb
+++ config/deploy/Test-test.rb
@@ -1,3 +1,2 @@
-"
-            server ec2-122-248-202-184.ap-southeast-1.compute.amazonaws.com, :app, :web, :db, :primary => true
-            set :rails_env, "production" 
+server ec2-122-248-202-184.ap-southeast-1.compute.amazonaws.com, :app, :web, :db, :primary => true
+set :rails_env, "production"
